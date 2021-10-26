41820795a537a77f813128ba0ca3199662fd9169
Boroda26 <luur@bk.ru>
2021-10-26 17:42:25 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+
+// App
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+    res.render('home', {title: 'Тестораскатки и лапшерезки IMPERIA'});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'О компании'});
+});
+
+
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`)
